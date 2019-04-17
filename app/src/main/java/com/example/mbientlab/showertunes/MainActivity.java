1855861ef26989c1a337ab71b9350a7276c0ec64
skyemakable <skyemakable@gmail.com>
2019-04-16 21:33:43 -0500

    Post troubleshooting with instructor.

--- app/src/main/java/com/example/mbientlab/showertunes/MainActivity.java
+++ app/src/main/java/com/example/mbientlab/showertunes/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Debug;
 import android.os.IBinder;
 import android.util.Log;
 import android.content.ComponentName;
@@ -35,6 +36,9 @@ import com.mbientlab.metawear.Subscriber;
 import com.mbientlab.metawear.android.BtleService;
 import com.mbientlab.metawear.builder.RouteBuilder;
 import com.mbientlab.metawear.builder.RouteComponent;
+import com.mbientlab.metawear.builder.filter.Comparison;
+import com.mbientlab.metawear.builder.filter.ThresholdOutput;
+import com.mbientlab.metawear.builder.function.Function1;
 import com.mbientlab.metawear.module.BarometerBosch;
 import com.mbientlab.metawear.module.Logging;
 import com.mbientlab.metawear.module.Temperature;
@@ -49,7 +53,7 @@ import java.util.Set;
 import bolts.Continuation;
 import bolts.Task;
 
-
+import com.mbientlab.metawear.module.Accelerometer;
 public class MainActivity extends Activity implements ServiceConnection {
 
     private static final String TAG = "MainActivity";
@@ -69,6 +73,17 @@ public class MainActivity extends Activity implements ServiceConnection {
     private MetaWearBoard board;
     private Logging logging;
     private BtleService.LocalBinder serviceBinder;
+    private Accelerometer accelerometer;
+
+    /* onCreate
+    * Set up objects for components of app. Set alpha of the metawear icon and speaker icon at low opacity.
+    * Get blueTooth adapter
+    * At startup, check if Bluetooth is on on the android phone. Display appropriate message.
+    * If status of Bluetooth changes, use bluetoothReceiver to listen and change text as appropriate
+    * Have Filter for listening for bluetooth speaker
+    * Create ArrayList for pairedDevices and look for specific Bluetooth speaker via its MAC address
+     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,17 +95,13 @@ public class MainActivity extends Activity implements ServiceConnection {
         AlbumArt = (ImageView) findViewById(R.id.AlbumArt);
         mediaPlayer = mediaPlayer.create(getApplicationContext(), R.raw.song); // TODO: Black Betty by Ram Jam
 
+        // Set opacity for both speaker and metawear icons to low
         BluetoothSpeaker.setAlpha(0.1f);
         Metawear.setAlpha(0.1f);
-
 
         // get bluetooth adapter
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         BluetoothDevice btSpeaker = null;
-
-
-        // Establish connection to the proxy
-        // bluetoothAdapter.getProfileProxy(this, profileListener, BluetoothProfile.A2DP);
 
         // Relevant when first starting the application
         if (bluetoothAdapter == null) {
@@ -105,20 +116,18 @@ public class MainActivity extends Activity implements ServiceConnection {
                 MusicText.setText("Connecting Devices...");
             }
         }
-        //Below is code for checking for changing in Bluetooth (enable/disable)
+        // Below is code for checking for changing in Bluetooth (enable/disable)
         IntentFilter changeFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-        registerReceiver(mBroadcastReceiver1, changeFilter);
+        registerReceiver(bluetoothReceiver, changeFilter);
 
+        // Have filter for connected bluetooth devices and receiver to help look for MAC address of desired speaker
         IntentFilter pairedBlueFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        pairedBlueFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        pairedBlueFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
         pairedBlueFilter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
-        pairedBlueFilter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED);
-        pairedBlueFilter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
         registerReceiver(deviceReceiver, pairedBlueFilter);
 
         // Get arraylist of paired Bluetooth devices
         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        // Boolean if the speaker we are looking at is found
         boolean found = false;
 
         if (pairedDevices.size() > 0) {
@@ -138,15 +147,14 @@ public class MainActivity extends Activity implements ServiceConnection {
             Log.i("BluetoothDevices", "Found speaker: " + btSpeaker.getAddress());
             //playMusic();
         }
-        // if not found
-        else {
-            // MusicText.setText("Bluetooth Speaker not found. Turn on and pair speaker");
-        }
-
+        // Metawear API
         getApplicationContext().bindService(new Intent(this, BtleService.class),
                 this, Context.BIND_AUTO_CREATE);
     }
 
+    /* onDestroy
+    * Unbind services, unregister receivers, and release the mediaPlayer
+    */
 
     @Override
     public void onDestroy() {
@@ -154,15 +162,20 @@ public class MainActivity extends Activity implements ServiceConnection {
 
         // Unbind the service when the activity is destroyed
         getApplicationContext().unbindService(this);
-        unregisterReceiver(mBroadcastReceiver1);
+        unregisterReceiver(bluetoothReceiver);
+        unregisterReceiver(deviceReceiver);
         mediaPlayer.release();
         mediaPlayer = null;
     }
+    /* onServiceConnected
+    * Binding for metawear api, and call retrieveBoard to get board and startup more metawear tasks/functions
+    */
 
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
         // Typecast the binder to the service's LocalBinder class
         serviceBinder = (BtleService.LocalBinder) service;
+        Log.i("metawear", serviceBinder.toString());
         Log.i("Metawear", "Service Connected");
 
         // mac addr here for metawear device
@@ -179,8 +192,12 @@ public class MainActivity extends Activity implements ServiceConnection {
         //Disconnect device
     }
 
+    /*
+       Broadcast receiver for changes made to Bluetooth on the android phone
+    */
+
     // Create a BroadcastReceiver for ACTION_FOUND. Relevant if change of Bluetooth status.
-    private final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
+    private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             // When discovery finds a device
@@ -193,19 +210,23 @@ public class MainActivity extends Activity implements ServiceConnection {
                         MusicText.setText("Bluetooth Disabled\nPlease turn on");
                         break;
                     case BluetoothAdapter.STATE_TURNING_OFF:
-                        Log.d(TAG, "mBroadcastReceiver1: STATE TURNING OFF");
+                        Log.d(TAG, "bluetoothReceiver: STATE TURNING OFF");
                         break;
                     case BluetoothAdapter.STATE_ON:
-                        Log.d(TAG, "mBroadcastReceiver1: STATE ON");;
+                        Log.d(TAG, "bluetoothReceiver: STATE ON");;
                         MusicText.setText("Connecting Devices...");
                         break;
                     case BluetoothAdapter.STATE_TURNING_ON:
-                        Log.d(TAG, "mBroadcastReceiver1: STATE TURNING ON");
+                        Log.d(TAG, "bluetoothReceiver: STATE TURNING ON");
                         break;
                 }
             }
         }
     };
+    /*
+    * Broadcast receiver looking for any newly added bluetooth devices. When Bluetooth speaker is turned on, this will
+    * Listen for it.
+    */
 
     private final BroadcastReceiver deviceReceiver = new BroadcastReceiver() {
         @Override
@@ -216,23 +237,16 @@ public class MainActivity extends Activity implements ServiceConnection {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 Log.d("BluetoothDevice", device.toString());
                 BluetoothSpeaker.setAlpha(1.0f);
+                // TODO: call metawear api to get values
                 playMusic();
-//                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-//                    Log.d("BluetoothDevice", "Device found");
-//                } else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
-//                    Log.d("BluetoothDevice", "Device now connected");
-//                } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-//                    Log.d("BluetoothDevice", "Device searching");
-//                } else if (BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED.equals(action)) {
-//                    Log.d("BluetoothDevice", "Device about to disconnect");
-//                } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
-//                    Log.d("BluetoothDevice", "Device disconnected");
-//                }
             }
         }
     };
 
-    // Play music when meta is found, bluetooth speaker found, and when temperature is high enough
+    /*
+    * playMusic called when Bluetooth speaker connected and (when written correctly)  metawear detects
+    * above required threshold in temperature. Makes edit in music text and image, and starts song.
+     */
     private void playMusic() {
         // play that funky music.
         MusicText.setText("Regina Spektor - \"Eet\" ");
@@ -240,7 +254,9 @@ public class MainActivity extends Activity implements ServiceConnection {
         mediaPlayer.start();
 
     }
-
+    /* retrieveBoard
+    *  Get board information, do the binding, get temperature data. Will add more comments when it works.
+     */
     public void retrieveBoard() {
 
         // btManager manages bluetooth connection
@@ -252,16 +268,50 @@ public class MainActivity extends Activity implements ServiceConnection {
 
         // create a new Metawear board object for the Bluetooth device
         board = serviceBinder.getMetaWearBoard(remoteDevice);
-        Log.i("MetawearBoard", "board is " + board.getModel());
-        Log.i ("MetawearBoard", String.valueOf(board.isConnected()));
+        Log.i("MetawearBoard", "in Retrieve Board: before getModel() call");
+        Log.i("MetawearBoard", String.valueOf(board.isConnected()));
+        board.connectAsync().onSuccessTask(task -> {
+            String LOG_TAG = "callback-for-accelerometer";
+            accelerometer = board.getModule(Accelerometer.class);
+            accelerometer.configure()
+                    .odr(50f)
+                    .commit();
+            return accelerometer.acceleration().addRouteAsync(source ->
+                    source.map(Function1.RSS).lowpass((byte) 4).filter(ThresholdOutput.BINARY, 0.5f)
+                            .multicast()
+                            .to().filter(Comparison.EQ, -1).log((data, env) -> Log.i(LOG_TAG, data.formattedTimestamp() + ": Entered Free Fall"))
+                            .to().filter(Comparison.EQ, 1).log((data, env) -> Log.i(LOG_TAG, data.formattedTimestamp() + ": Left Free Fall"))
+                            .end());
+        }).continueWith((Continuation<Route, Void>) task -> {
+            if (task.isFaulted()) {
+                Log.e(LOG_TAG, board.isConnected() ? "Error setting up route" : "Error connecting", task.getError());
+            } else {
+                Log.i(LOG_TAG, "Connected");
+                debug = board.getModule(Debug.class);
+                logging= board.getModule(Logging.class);
+            }
+
+            return null;
+        });
+        /*if(board.getModel()!=null){
+
+            Log.i("MetawearBoard", "board is " + String.valueOf(board.getModel()));
+        }
+        else{
+            Log.i("MetawearBoard", "board is null");
+        }*/
+
+        //Log.i ("MetawearBoard", String.valueOf(board.isConnected()));
+        //Log.i("MetawearBoard", "in Retrieve Board after getModel() call");
 
         // Establishes a BLE connection to MetaWear board
-        board.connectAsync().onSuccess(new Continuation<Void, Void>() {
+        /*board.connectAsync().onSuccessTask(new Continuation<Void, Void>() {
             @Override
             public Void then(Task <Void> task) throws Exception {
+                Log.i("showerTunes", task.toString());
                 Log.i("ShowerTunes", "Connected to " + MAC_ADDR);
 
-                logging = board.getModule(Logging.class);
+                /*logging = board.getModule(Logging.class);
 
                 // create a reference to the temperature sensor on the board
                 final Temperature temp = board.getModule(Temperature.class);
@@ -290,14 +340,7 @@ public class MainActivity extends Activity implements ServiceConnection {
                 });
                 return null;
             }
-        });
+        }); */
     }
 
-    // Make device discoverable? https://stackoverflow.com/questions/46841534/pair-a-bluetooth-device-in-android-studio
-    private void makeDiscoverable() {
-        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-        startActivity(discoverableIntent);
-        Log.i("Log", "Discoverable");
-    }
 }
